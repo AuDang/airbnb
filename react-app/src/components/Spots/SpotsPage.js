ba5a7e5e6b974b62e0a33ea0537c52f3c88aa21f
Austin Dang <austindang415@gmail.com>
2022-07-08 13:53:21 -0700

    fixed bugs

--- react-app/src/components/Spots/SpotsPage.js
+++ react-app/src/components/Spots/SpotsPage.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllSpots } from '../../store/spot';
 
 const SpotsPage = () => {
+   const spots = useSelector(state => state.spotReducer)
+   console.log('spots', spots)
+
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      dispatch(getAllSpots())
+   },[dispatch])
+
    return (
-      <>
+      <div>
       hello
-      </>
+      </div>
    )
 }
 
