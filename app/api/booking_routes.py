b832a74bfd4fc6bb58556fc2ecaf6ac7a00cdfac
Austin Dang <austindang415@gmail.com>
2022-08-04 23:22:10 -0700

    bookings troute

--- app/api/booking_routes.py
+++ app/api/booking_routes.py
@@ -17,4 +17,31 @@ def validation_errors_to_error_messages(validation_errors):
     return errorMessages
 
 
-# @booking_routes.route("/users/")
+@booking_routes.route('/users/<int:id>')
+def user_bookings(id):
+    bookings = Booking.query.filter(Booking.user_id == id)
+    return jsonify([booking.to_dict() for booking in bookings])
+
+
+@booking_routes.route('/spot/<int:id>' , methods=['POST'])
+@login_required
+def create_booking(id):
+    form = BookingForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_booking = Booking()
+        form.populate_obj(new_booking)
+        db.session.add(new_booking)
+        db.session.commit()
+
+        return new_booking.to_dict()
+    else:
+        return {"errors": validation_errors_to_error_messages(form.errors)}, 400
+
+@booking_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_booking(id):
+    delete_booking = Booking.query.get(id)
+    db.session.delete(delete_booking)
+    db.session.commit()
+    return delete_booking.to_dict()
