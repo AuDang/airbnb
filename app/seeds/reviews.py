f1ec9e7e6ed7d11e8129a452465454fff532f045
Austin Dang <austindang415@gmail.com>
2022-09-05 09:27:02 -0700

    change some seeder files

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -2,32 +2,32 @@ from app.models import db, Review
 
 def seed_reviews():
    review1 = Review(
-      user_id=2,
+      user_id=3,
       spot_id=1,
       review='This place just can\'t get any better and the view is amazing',
       rating=5
    )
    review2 = Review(
-      user_id=1,
+      user_id=3,
       spot_id=3,
       review='This place is DOPE !',
       rating=5
    )
    review3 = Review(
-      user_id=1,
+      user_id=2,
       spot_id=3,
       review='What a lovely little place in near the heart of San Francisco and the Host was amazing with communication.',
       rating=5
    )
 
    review4 = Review(
-      user_id=2,
-      spot_id=3,
+      user_id=3,
+      spot_id=1,
       review='Other than the price, this place is great.',
       rating=4
    )
    review5 = Review(
-      user_id=1,
+      user_id=3,
       spot_id=5,
       review='This place is amazing!',
       rating=5
